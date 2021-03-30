05d8cee1132c902bb91ded509d1ed6e28d8fc78d
vibhuti02 <vibhutidesai02@gmail.com>
2021-03-30 18:50:23 +0530

    Update validation.js

--- validation.js
+++ validation.js
@@ -30,7 +30,7 @@
         var uid_len = uid.value.length;  
         if (uid_len == 0 || uid_len >= my || uid_len < mx)  
         {  
-            alert("User Id should not be empty / length be between "+mx+" to "+my);  
+            alert("Name should not be empty / length be between "+mx+" to "+my);  
             uid.focus();  
             return false;  
         }  
